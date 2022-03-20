f95d93fa3eaeda25f427be50b033c2300bd045fd
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2022-03-20 10:39:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+
+const input = document.querySelector('input');
+
+// Binds an event listener in addingEventListener()
 function addingEventListener() {
+	input.addEventListener('click', function(event) {
+		alert('I was clicked!');
+	});
 }
